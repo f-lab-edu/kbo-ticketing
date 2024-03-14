7ad8f73408fb3e7615f20a0a9c0b1b4a5e168bd8
hazel <waithjno@gmail.com>
2024-03-14 15:03:13 +0900

    feat : initial commit

---
+++ src/main/java/com/kboticketing/kboticketing/KboTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.kboticketing.kboticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KboTicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KboTicketingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kboticketing/kboticketing/KboTicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kboticketing.kboticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KboTicketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
