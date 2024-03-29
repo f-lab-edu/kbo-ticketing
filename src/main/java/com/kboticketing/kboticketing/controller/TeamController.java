60fa413096c940b3715fe273a33560f8e20a4831
hazel <waithjno@gmail.com>
2024-03-29 19:35:51 +0900

    [#14] feat : 팀 리스트 API 기능 구현

---
+++ src/main/java/com/kboticketing/kboticketing/controller/TeamController.java
@@ -0,0 +1,26 @@
+package com.kboticketing.kboticketing.controller;
+
+import com.kboticketing.kboticketing.domain.Team;
+import com.kboticketing.kboticketing.service.TeamService;
+import com.kboticketing.kboticketing.utils.response.CommonResponse;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hazel
+ */
+@RestController
+@RequiredArgsConstructor
+public class TeamController {
+
+    private final TeamService teamService;
+
+    @GetMapping("teams")
+    public ResponseEntity<CommonResponse> getTeams() {
+        List<Team> teams = teamService.getTeams();
+        return ResponseEntity.ok(CommonResponse.ok(teams));
+    }
+}
---
+++ src/main/java/com/kboticketing/kboticketing/service/TeamService.java
@@ -0,0 +1,21 @@
+package com.kboticketing.kboticketing.service;
+
+import com.kboticketing.kboticketing.dao.TeamMapper;
+import com.kboticketing.kboticketing.domain.Team;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author hazel
+ */
+@Service
+@RequiredArgsConstructor
+public class TeamService {
+
+    private final TeamMapper teamMapper;
+
+    public List<Team> getTeams() {
+        return teamMapper.selectTeams();
+    }
+}
