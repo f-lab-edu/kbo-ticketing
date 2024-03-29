d5571df8b766b10823a6c076e4311b42c3baa76f
hazel <waithjno@gmail.com>
2024-03-29 12:34:21 +0900

    [#2] refactor : SecureRandom private final로 수정

--- src/main/java/com/kboticketing/kboticketing/utils/NumberUtils.java
+++ src/main/java/com/kboticketing/kboticketing/utils/NumberUtils.java
@@ -8,10 +8,10 @@ import java.security.SecureRandom;
 public class NumberUtils {
 
     private static final int CODE_LENGTH = 6;
+    private static final SecureRandom secureRandom = new SecureRandom();
 
     public static String createRandomNumber() {
         StringBuilder code = new StringBuilder();
-        SecureRandom secureRandom = new SecureRandom();
 
         for (int i = 0; i < CODE_LENGTH; i++) {
             int randomNumber = secureRandom.nextInt(10); //0부터 9까지 랜덤한 정수 반환
