95b06f159a917e441c0a79322d04172de2154f44
hazel <waithjno@gmail.com>
2024-03-30 14:10:00 +0900

    [#15] feat : 경기 목록 조회 API 구현

---
+++ src/main/java/com/kboticketing/kboticketing/dao/ScheduleMapper.java
@@ -0,0 +1,15 @@
+package com.kboticketing.kboticketing.dao;
+
+import com.kboticketing.kboticketing.domain.ScheduleTeam;
+import com.kboticketing.kboticketing.dto.ScheduleQueryParamDto;
+import java.util.ArrayList;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author hazel
+ */
+@Mapper
+public interface ScheduleMapper {
+
+    ArrayList<ScheduleTeam> selectSchedules(ScheduleQueryParamDto scheduleQueryParamDto);
+}
