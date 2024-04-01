1cbe74f20aaa97271f70abe95bdddc7bbb048e6f
hazel <waithjno@gmail.com>
2024-04-01 22:14:48 +0900

    [#15] refactor : ScheduleQueryParamDto 수정

--- src/main/java/com/kboticketing/kboticketing/dto/ScheduleQueryParamDto.java
+++ src/main/java/com/kboticketing/kboticketing/dto/ScheduleQueryParamDto.java
@@ -17,8 +17,8 @@ public class ScheduleQueryParamDto {
     public ScheduleQueryParamDto(Integer teamId, Integer month, Integer day, Integer stadiumId) {
         this.teamId = teamId;
         // month 필드가 null이라면 현재 월 설정
-        this.month = month != null ? month : LocalDateTime.now()
-                                                          .getMonthValue();
+        this.month = month == null ? LocalDateTime.now()
+                                                  .getMonthValue() : month;
         this.day = day;
         this.stadiumId = stadiumId;
     }
