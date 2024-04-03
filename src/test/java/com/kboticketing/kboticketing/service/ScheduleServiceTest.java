a67b5c423a6b4c5257805bb23e455af6ecfd7ec4
hazel <waithjno@gmail.com>
2024-04-03 18:13:50 +0900

    [#18] test : 경기 정보, 좌석 목록 조회 service 유닛 테스트 추가

--- src/test/java/com/kboticketing/kboticketing/service/ScheduleServiceTest.java
+++ src/test/java/com/kboticketing/kboticketing/service/ScheduleServiceTest.java
@@ -4,7 +4,9 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 
 import com.kboticketing.kboticketing.dao.ScheduleMapper;
+import com.kboticketing.kboticketing.domain.ScheduleInfo;
 import com.kboticketing.kboticketing.domain.ScheduleTeam;
+import com.kboticketing.kboticketing.domain.SeatGradeBySchedule;
 import java.util.ArrayList;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -38,5 +40,31 @@ class ScheduleServiceTest {
 
         //when, then
         assertThat(scheduleService.getSchedules(any())).isEqualTo(scheduleTeams);
+    }
+
+    @Test
+    @DisplayName("[SUCCESS] 경기 정보 조회 테스트")
+    public void getScheduleTest() {
+
+        //given
+        ScheduleInfo scheduleInfo = new ScheduleInfo(1, "LG vs KT", "2024-03-29 18:30:00",
+            "서울종합운동장 야구장");
+        given(scheduleMapper.selectInfo(any())).willReturn(scheduleInfo);
+
+        //when, then
+        assertThat(scheduleService.getSchedule(any())).isEqualTo(scheduleInfo);
+    }
+
+    @Test
+    @DisplayName("[SUCCESS] 좌석 목록 조회 테스트")
+    public void getSeatGradeTest() {
+
+        //given
+        ArrayList<SeatGradeBySchedule> seatGradeList = new ArrayList<>();
+        seatGradeList.add(new SeatGradeBySchedule(1, 1, "블루석", "20000", "500"));
+        given(scheduleMapper.selectSeatGrade(any())).willReturn(seatGradeList);
+
+        //when, then
+        assertThat(scheduleService.getSeatGradeBySchedule(any())).isEqualTo(seatGradeList);
     }
 }
