15a1daff9a1ac7d345fed5c3c96c71f35c8f08bf
hazel <waithjno@gmail.com>
2024-04-09 23:39:19 +0900

    [#26] feat : 예매 관련 에러코드 추가

--- src/main/java/com/kboticketing/kboticketing/utils/exception/ErrorCode.java
+++ src/main/java/com/kboticketing/kboticketing/utils/exception/ErrorCode.java
@@ -18,9 +18,15 @@ public enum ErrorCode {
     FREQUENT_VERIFICATION_REQUEST(HttpStatus.BAD_REQUEST, "잦은 인증번호 요청은 불가능합니다."),
     REQUEST_VERIFICATION_FIRST(HttpStatus.BAD_REQUEST, "인증을 먼저 진행해주세요."),
     WRONG_VERIFICATION_CODE(HttpStatus.BAD_REQUEST, "인증번호가 다릅니다."),
+    SEAT_IN_PROGRESS(HttpStatus.BAD_REQUEST, "이미 선택된 좌석입니다."),
+    SELECT_SEAT(HttpStatus.BAD_REQUEST, "자리를 선택해주세요."),
+    MAXIMUM_SEAT_EXCEED(HttpStatus.BAD_REQUEST, "예매 가능한 수량을 초과했습니다."),
+    MAXIMUM_RESERVATION_EXCEED(HttpStatus.BAD_REQUEST, "한 경기당 예매 가능한 횟수를 초과했습니다."),
+    RESERVED_SEAT(HttpStatus.BAD_REQUEST, "이미 예매된 좌석입니다."),
 
     /* 500 INTERNAL_SERVER_ERROR : 서버 오류 */
-    FAIL_SEND_EMAIL(HttpStatus.INTERNAL_SERVER_ERROR, "이메일 전송에 실패하였습니다.");
+    FAIL_SEND_EMAIL(HttpStatus.INTERNAL_SERVER_ERROR, "이메일 전송에 실패하였습니다."),
+    RESERVED_PROCESS_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "예매 중 문제가 발생하였습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
