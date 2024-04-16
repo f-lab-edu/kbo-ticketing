746360ba22a6d8c22d39b212f19206ed961dc61a
hazel <waithjno@gmail.com>
2024-04-16 19:33:55 +0900

    [#30] test : DateTimeUtils unit test 추가

---
+++ src/test/java/com/kboticketing/kboticketing/utils/DateTimeUtilsTest.java
@@ -0,0 +1,23 @@
+package com.kboticketing.kboticketing.utils;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author hazel
+ */
+public class DateTimeUtilsTest {
+
+    @Test
+    @DisplayName("[SUCCESS] 시간 formatter 테스트")
+    public void getCurrentTimeTest() {
+
+        //when
+        String currentTime = DateTimeUtils.getCurrentTime();
+
+        //then
+        assertNotNull(currentTime);
+    }
+}
