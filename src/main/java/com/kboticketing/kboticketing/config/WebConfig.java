4b572b7ff2514cbbf1be4a2afbf9e7536bfee3ab
hazel <waithjno@gmail.com>
2024-05-06 19:37:04 +0900

    [#35] feat : LoginInterceptor,WebConfig 수정

--- src/main/java/com/kboticketing/kboticketing/config/WebConfig.java
+++ src/main/java/com/kboticketing/kboticketing/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.kboticketing.kboticketing.config;
 
-import com.kboticketing.kboticketing.Interceptor.LoginInterceptor;
+import com.kboticketing.kboticketing.interceptor.LoginInterceptor;
+import com.kboticketing.kboticketing.service.JwtService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,11 +11,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author hazel
  */
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final JwtService jwtService;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor())
+        registry.addInterceptor(new LoginInterceptor(jwtService))
                 .order(1)
                 .addPathPatterns("/", "/seats", "/reservations");
     }
