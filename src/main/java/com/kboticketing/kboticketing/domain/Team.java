25f8a7b3093c00fedb35a8b79ece216245baf620
hazel <waithjno@gmail.com>
2024-05-29 15:45:05 +0900

    [#44] feat : 팀 목록 조회 캐싱 추가 및 도메인 수정

--- src/main/java/com/kboticketing/kboticketing/domain/Team.java
+++ src/main/java/com/kboticketing/kboticketing/domain/Team.java
@@ -1,15 +1,21 @@
 package com.kboticketing.kboticketing.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 /**
  * @author hazel
  */
-@RequiredArgsConstructor
 @Getter
 public class Team {
 
     private final Integer teamId;
     private final String name;
+
+    @JsonCreator
+    public Team(@JsonProperty("teamId") Integer teamId, @JsonProperty("name") String name) {
+        this.teamId = teamId;
+        this.name = name;
+    }
 }
--- src/main/java/com/kboticketing/kboticketing/service/TeamService.java
+++ src/main/java/com/kboticketing/kboticketing/service/TeamService.java
@@ -4,6 +4,7 @@ import com.kboticketing.kboticketing.dao.TeamMapper;
 import com.kboticketing.kboticketing.domain.Team;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,6 +16,7 @@ public class TeamService {
 
     private final TeamMapper teamMapper;
 
+    @Cacheable(value = "teams/")
     public List<Team> getTeams() {
         return teamMapper.selectTeams();
     }
