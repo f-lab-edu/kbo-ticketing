ba422568d7429e33169643b0293ca3c49ca60013
hazel <waithjno@gmail.com>
2024-09-04 23:04:54 +0900

    [#46] chore : queryDSL 의존성 추가 및 설정

---
+++ src/main/java/com/kboticketing/kboticketing/config/QueryDSLConfig.java
@@ -0,0 +1,23 @@
+package com.kboticketing.kboticketing.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author hazel
+ */
+@Configuration
+public class QueryDSLConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
